46e4c3c3b63dbd6de8564de2bf4dd2eaa057d460
Abu Amar <amarjabon130@gmail.com>
2022-02-17 08:54:15 +0700

    membuat program untuk mengetahui provider dari sebuah nomor

--- phone_number.py
+++ phone_number.py
@@ -1,1 +1,13 @@
+import phonenumbers
+from phonenumbers import geocoder, carrier, timezone
 
+phone_number = phonenumbers.parse(input("Masukkan nomor: (example : +62....) -> "))
+
+# this will print the country name
+print(f"Negara : {geocoder.description_for_number(phone_number, 'en')}")
+
+# this will print the service provider
+print(f"Provider : {carrier.name_for_number(phone_number, 'en')}")
+
+# this will print the timezone
+print(f"Lokasi : {timezone.time_zones_for_geographical_number(phone_number)}") 
