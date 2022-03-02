99d60214584df6ac41af049ad84037b136139e38
Abu Amar <amarjabon130@gmail.com>
2022-03-02 13:56:38 +0700

    program jadiiii :)

--- closure_and_decorator.py
+++ closure_and_decorator.py
@@ -1,4 +1,5 @@
 def check_number(func):
+    "untuk check alas dan tinggi, jika kurang dari 5 akan diganti dengan 5"
     def inner(a, t):
         if a < 5:
             print("alas kurang dari 5, maka diganti dengan 5")
@@ -11,7 +12,10 @@ def check_number(func):
 
 @check_number
 def hitung_luas(a, t):
+    "function untuk menghitung luas dari segitiga"
     luas = 1/2 * a * t
-    print(luas)
+    return luas
 
-hitung_luas(4, 8)
+segitiga_1 = hitung_luas(4, 8)
+
+print(f"Luas dari segitiga 1 : {round(segitiga_1)}")
