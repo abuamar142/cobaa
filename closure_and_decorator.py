c69c478fa3c2e14632ef133958f41b2df3ad7e10
Abu Amar <amarjabon130@gmail.com>
2022-03-04 22:42:49 +0700

    tugas modul closure and decorator

--- closure_and_decorator.py
+++ closure_and_decorator.py
@@ -1,11 +1,14 @@
+a = float(input("Masukkan alas segitiga: "))
+t = float(input("Masukkan tinggi segitiga: "))
+
 def check_number(func):
     "untuk check alas dan tinggi, jika kurang dari 5 akan diganti dengan 5"
     def inner(a, t):
         if a < 5:
-            print("alas kurang dari 5, maka diganti dengan 5")
+            print("Alas kurang dari 5, maka diganti dengan 5")
             a = 5
         if t < 5:
-            print("tinggi kurang dari 5, maka diganti dengan 5")
+            print("Tinggi kurang dari 5, maka diganti dengan 5")
             t = 5
         return func(a, t)
     return inner
@@ -16,6 +19,6 @@ def hitung_luas(a, t):
     luas = 1/2 * a * t
     return luas
 
-segitiga_1 = hitung_luas(4, 8)
+segitiga = hitung_luas(a, t)
 
-print(f"Luas dari segitiga 1 : {round(segitiga_1)}")
+print(f"Luas dari segitiga : {round(segitiga)}")
